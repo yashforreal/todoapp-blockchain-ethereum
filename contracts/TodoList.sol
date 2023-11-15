e265c3a31fbe976ba9bf10984dd2420312422a69
Yash <yka.yash@gmail.com>
2023-11-15 23:30:26 +0530

    added contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -12,12 +12,31 @@ contract TodoList {
 
     mapping(uint => Task) public tasks;
 
+    event TaskCreated (
+        uint id,
+        string content,
+        bool completed
+    );
+
+    event TaskCompleted (
+        uint id,
+        bool completed
+    );
+
     constructor() public {
-        createTask("Create a new task");
+        createTask("Create tasks");
     }
 
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
     }
 }
